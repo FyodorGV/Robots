ceeeb5a8af51f53544e8fa9f55cd2f7991c4ac49
FyodorGV <fedorgreben17@mail.ru>
2026-05-19 07:27:47 +0300

    Добавил класс для сохранения параметров и работы с файлом. Убрал лишние строчки и подправил ошибки

---
+++ src/main/java/gui/SessionManager.java
@@ -0,0 +1,54 @@
+package gui;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * Класс для сохранения и загрузки состояния окон в файл
+ */
+public class SessionManager {
+
+    private final String path;  /** Полный путь к файлу конфигурации */
+
+    /**
+     * Создает объект для работы с файлом состояния
+     */
+    public SessionManager(String surname) {
+        this.path = System.getProperty("user.home") + "/" + surname + "/state.cfg";
+        new File(path).getParentFile().mkdirs();
+    }
+
+    /**
+     * Сохраняет словарь с данными в файл
+     */
+    public void save(Map<String, String> data) {
+        Properties props = new Properties();
+        props.putAll(data);
+        try (FileOutputStream out = new FileOutputStream(path)) {
+            props.store(out, "состояние окон");
+        } catch (IOException e) {}
+    }
+
+    /**
+     * Загружает данные из файла в словарь
+     */
+    public Map<String, String> load() {
+        Map<String, String> data = new HashMap<>();
+        File file = new File(path);
+        if (!file.exists()) return data;
+
+        Properties props = new Properties();
+        try (FileInputStream in = new FileInputStream(file)) {
+            props.load(in);
+        } catch (IOException e) {
+            return data;
+        }
+
+        for (String key : props.stringPropertyNames()) {
+            data.put(key, props.getProperty(key));
+        }
+        return data;
+    }
+}
